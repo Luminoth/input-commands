d2154798844c20081f8ee470fdddc303aecf16b0
Shane Lillie <ignignokterr@gmail.com>
2026-03-01 10:15:29 -0800

    Add interactable terminal

--- scripts/Terminal.cs
+++ scripts/Terminal.cs
@@ -3,15 +3,31 @@ using Godot;
 // TODO:
 public partial class Terminal : Node
 {
-    public void OnInteract(Node player)
+    /*public void OnInteract(Node player)
     {
         // switch actor to drone (TODO: I don't like this)
-        //InputManager.Instance.Actor = GetNode<Node>("../Drone");
+        InputManager.Instance.Actor = GetNode<Node>("../Drone");
 
         // update the input context
-        //InputManager.Instance.PushContext(ResourceLoader.Load<InputContext>("res://DroneContext.tres"));
+        InputManager.Instance.PushContext(ResourceLoader.Load<InputContext>("res://DroneContext.tres"));
 
         // update to the drone camera
-        //GetNode<Camera3D>("../Drone/Camera").MakeCurrent();
+        GetNode<Camera3D>("../Drone/Camera").MakeCurrent();
+    }*/
+
+    public void _on_interact_body_entered(Node3D body)
+    {
+        if (body is Player player)
+        {
+            GD.Print("player enter");
+        }
+    }
+
+    public void _on_interact_body_exited(Node3D body)
+    {
+        if (body is Player player)
+        {
+            GD.Print("player exit");
+        }
     }
 }
